1f0cb928c8100296e6375c7690d4e41118e42d18
giorgi <gmike19@freeuni.edu.ge>
2021-07-04 14:35:21 +0400

    Overrided doGet and doPost

--- src/main/java/servlets/register/RegisterServlet.java
+++ src/main/java/servlets/register/RegisterServlet.java
@@ -1,4 +1,19 @@
 package servlets.register;
 
-public class RegisterServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
