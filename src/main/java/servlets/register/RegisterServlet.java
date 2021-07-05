9b52b5ddc0f863187a9d1780159d56561a0ab0e0
giorgi <gmike19@freeuni.edu.ge>
2021-07-05 16:47:11 +0400

    Added code for registration attempt

--- src/main/java/servlets/register/RegisterServlet.java
+++ src/main/java/servlets/register/RegisterServlet.java
@@ -3,6 +3,7 @@ package servlets.register;
 import commons.beans.UserBean;
 import dao.implementation.UserDAO;
 import dao.interfaces.UserDAOInterface;
+import model.UserUtility;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,6 +18,10 @@ import java.sql.Date;
 
 @WebServlet("/register-attempt")
 public class RegisterServlet extends HttpServlet {
+
+    public static final String SUCCESS_STR = "success";
+    public static final String FAILED_STR = "failed";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
@@ -30,34 +35,14 @@ public class RegisterServlet extends HttpServlet {
         String surname = req.getParameter("surname");
         String password = req.getParameter("password");
         Date date = Date.valueOf(req.getParameter("birthday"));
-        String hash = generateHash(password);
+        String hash = UserUtility.generateHash(password);
         UserBean user = new UserBean(username, name, surname, hash, date);
+        String state = SUCCESS_STR;
         if (userDao.addUser(user)) {
-            //req.getRequestDispatcher("").forward(req, resp);
+            req.getSession().setAttribute(UserBean.USER_ATTR, user);
         } else {
-            // display error message
+            state = FAILED_STR;
         }
-
-    }
-
-    private String generateHash(String password) {
-        MessageDigest md = null;
-        byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
-        try {
-            md = MessageDigest.getInstance("SHA-1");
-            md.update(passwordBytes);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        assert md != null;
-        byte[] bytes = md.digest();
-        StringBuilder builder = new StringBuilder();
-        for (int aByte : bytes) {
-            int val = aByte;
-            val = val & 0xff;  // remove higher bits, sign
-            if (val < 16) builder.append('0'); // leading 0
-            builder.append(Integer.toString(val, 16));
-        }
-        return builder.toString();
+        resp.getWriter().print(state);
     }
 }
