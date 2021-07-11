d2849f52f642701fc8437e7ebc05e46c592858b7
Luka Tsilosani <ltsil19@freeuni.edu.ge>
2021-07-11 15:09:55 +0400

    removed test servlet from mapping

--- src/main/java/servlets/matchmaking/MatchmakingTestServlet.java
+++ src/main/java/servlets/matchmaking/MatchmakingTestServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/test_matchmaking")
 public class MatchmakingTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
