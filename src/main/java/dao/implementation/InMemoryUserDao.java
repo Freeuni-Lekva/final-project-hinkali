5ecf18b67fa316daa226d6a7c68ab3340784b605
Luka <wilosani1808@gmail.com>
2021-07-19 06:05:26 -0700

    user in memory dao implementation, no filtering

---
+++ src/main/java/dao/implementation/InMemoryUserDao.java
@@ -0,0 +1,68 @@
+package dao.implementation;
+
+import commons.beans.UserBean;
+import dao.interfaces.Filter;
+import dao.interfaces.UserDAOInterface;
+
+import javax.xml.registry.infomodel.User;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InMemoryUserDao implements UserDAOInterface {
+    
+    private List<UserBean> users;
+    
+    @Override
+    public boolean addUser(UserBean user) {
+        users.add(user);
+        return true;
+    }
+
+    @Override
+    public UserBean getUser(String username, String password) {
+        List<UserBean> results = users.stream().filter(userBean -> userBean.getUsername().equals(username) && userBean.getPassword().equals(password)).collect(Collectors.toList());
+        if (results.isEmpty()) return null;
+        return results.get(0);
+    }
+
+    @Override
+    public UserBean getUserById(int id) {
+        List<UserBean> results = users.stream().filter(userBean -> userBean.getId() == id).collect(Collectors.toList());
+        if (results.isEmpty()) return null;
+        return results.get(0);
+    }
+
+    @Override
+    public boolean removeUser(int id) {
+        for (UserBean u :
+                users) {
+            if (u.getId() == id) {
+                users.remove(u);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    // return all
+    @Override
+    public List<UserBean> getUsersWithFilter(Filter f) {
+        return users;
+    }
+
+    @Override
+    public boolean changeUser(UserBean user) {
+        int index = -1;
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == user.getId()){
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) return false;
+        users.set(index, user);
+
+        return false;
+    }
+}
