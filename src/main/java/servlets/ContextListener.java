836e5cd0441afc789dba8c503eabf1878d479fb3
Luka <wilosani1808@gmail.com>
2021-07-20 11:12:40 +0400

    added to context listener

--- src/main/java/servlets/ContextListener.java
+++ src/main/java/servlets/ContextListener.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import dao.implementation.FriendDao;
+import dao.implementation.InMemoryFriendDao;
+import dao.implementation.InMemoryUserWrapperDao;
 import dao.implementation.UserWrapperDao;
 import dao.interfaces.FriendDaoInterface;
 import dao.interfaces.UserWrapperInterface;
@@ -12,12 +14,17 @@ import javax.servlet.ServletContextListener;
 
 public class ContextListener implements ServletContextListener {
     public static final boolean TESTING = true;
+    public static final boolean IN_MEMORY = false;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("Context is being initialized");
 
         addMatchmakingQueue(sce);
+        if (IN_MEMORY){
+            setupInMemoryDaos(sce);
+            return;
+        }
 
         UserWrapperInterface userWrapperDao = new UserWrapperDao();
         sce.getServletContext().setAttribute(UserWrapperDao.USER_WRAPPER_ATTR, userWrapperDao);
@@ -25,6 +32,13 @@ public class ContextListener implements ServletContextListener {
 
         FriendDaoInterface friendDao = new FriendDao();
         sce.getServletContext().setAttribute(FriendDao.FRIEND_DAO_ATTR, friendDao);
+    }
+
+    private void setupInMemoryDaos(ServletContextEvent sce) {
+        UserWrapperInterface wrapperInterface = new InMemoryUserWrapperDao();
+        FriendDaoInterface friendDaoInterface = new InMemoryFriendDao();
+        sce.getServletContext().setAttribute(UserWrapperDao.USER_WRAPPER_ATTR,  wrapperInterface);
+        sce.getServletContext().setAttribute(FriendDao.FRIEND_DAO_ATTR, friendDaoInterface);
     }
 
     private void addMatchmakingQueue(ServletContextEvent sce) {
