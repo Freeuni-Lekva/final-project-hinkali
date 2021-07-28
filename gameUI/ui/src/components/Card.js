e72a112b8c54bb21acaf314595a0a4634a254123
Luka <wilosani1808@gmail.com>
2021-07-28 11:52:26 +0400

    some color changes

--- gameUI/ui/src/components/Card.js
+++ gameUI/ui/src/components/Card.js
@@ -1,9 +1,24 @@
 import '../styles/Card.css'
 import notFoundImg from '../resources/image-not-found.png'
 
-const Card = ({card}) => {
+const colors = ['#78909c','#494ca2','darkred','#febf0a']
+const pickColor = (rating) => {
+    switch (true){
+        case (rating <= 4):
+            return colors[0]
+        case (rating <= 8):
+            return colors[1]
+        case (rating <= 12):
+            return colors[2]
+        default:
+            return colors[3]
+    }
+}
 
-    return <div className="Card">
+const Card = ({card}) => {
+    let pickedColor = pickColor(card.rating);
+
+    return <div className="Card" style={{backgroundColor: pickedColor}}>
         <div className="info">
             <label className="ratingLabel">
                 {card.rating}
