db463f5809098cafee421cf4979c7232c7f66cbb
Luka <wilosani1808@gmail.com>
2021-07-29 10:57:56 +0400

    update UserBean equals method

--- src/main/java/commons/beans/UserBean.java
+++ src/main/java/commons/beans/UserBean.java
@@ -1,6 +1,7 @@
 package commons.beans;
 
 import java.sql.Date;
+import java.util.Objects;
 
 public class UserBean {
     // put this in setAttribute(key, obj)
@@ -73,10 +74,14 @@ public class UserBean {
 
     @Override
     public boolean equals(Object o) {
-        if (o instanceof UserBean) {
-            UserBean toCompare = (UserBean) o;
-            return this.id == toCompare.id;
-        }
-        return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserBean userBean = (UserBean) o;
+        return id == userBean.id && Objects.equals(username, userBean.username) && Objects.equals(name, userBean.name) && Objects.equals(surname, userBean.surname) && Objects.equals(password, userBean.password) && Objects.equals(birthday, userBean.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, name, surname, password, birthday);
     }
 }
