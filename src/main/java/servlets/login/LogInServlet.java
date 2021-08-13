e7f9629719c6b18ca7de7f3d27f0dbfd89a53eb0
eleneormots <eormo19@freeuni.edu.ge>
2021-08-13 18:37:11 +0400

    login page with error(Does not work if you enter the wrong username or password)

--- src/main/java/servlets/login/LogInServlet.java
+++ src/main/java/servlets/login/LogInServlet.java
@@ -1,6 +1,5 @@
 package servlets.login;
 
-
 import commons.beans.UserBean;
 import dao.implementation.UserDAO;
 import dao.interfaces.UserDAOInterface;
@@ -10,12 +9,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet("/login")
 public class LogInServlet extends HttpServlet {
     public static final String SUCCESS_STR = "success";
     public static final String FAILED_STR = "failed";
+    public static final String PASSWORD_FAILED_STR = "Pfailed";
+
+
+
+
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("login.html").forward(req, resp);
@@ -28,22 +35,40 @@ public class LogInServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String state;
-        boolean success= false;
 
-        UserBean currUser = userDao.getUser( username, password);
 
-        if(currUser!=null){
-            success= true;
-        }
-        if(success){
+
+            UserBean currUser = userDao.getUser(username, password);
+            UserBean userByName= userDao.getUserByUsername(username);
+
+
+        if(currUser != null){
             state = SUCCESS_STR;
             resp.sendRedirect("home");
-            //req.getRequestDispatcher("WEB-INF/home/home.jsp").forward(req,resp);
+
         }else{
             state = FAILED_STR;
         }
+        if(currUser==null&&userByName!=null)
+            state=PASSWORD_FAILED_STR;
         resp.getWriter().print(state);
-
-
     }
+    /*
+    boolean emptyFieldsDetector(String username, String password){
+
+        if (username.equals("")) {
+            JOptionPane.showMessageDialog(null, "Field for username is empty");
+            return false;
+        }
+        if (password.equals("")) {
+            JOptionPane.showMessageDialog(null, "Filed for password is empty");
+            return false;
+        }
+        return true;
+    }
+
+
+
+     */
+
 }
--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -1,9 +1,38 @@
-const menu= document.querySelector('#mobile-menu')
+/*const menu=
+    document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector(
-    'navbar__menu')
+    '.navbar__menu')
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 
 })
+
+
+ */
+
+
+$(function(){
+    $("#login--form").on("submit", function (event) {
+        event.preventDefault();
+
+        $.post("login",  function (resp) {
+            if(resp==="failed"){
+                window.location = "/login";
+            }
+            else if (resp === "success") {
+                window.location = "/home";
+            }else if(resp==="Pfailed") {
+                let msg = $("#info");
+                msg.html("Password is wrong, try again!");
+                msg.css('color', 'red');
+                $("#password").css('border-color', 'red');
+            }
+        });
+    });
+});
+
+
+
+
